a9a97682a99416513b0acf54d3991cafe3a5e103
Ronatas <ronatas.diniz@gmail.com>
2024-01-13 16:10:36 -0300

    Changes to be committed: 	modified:   css/global.css 	modified:   css/pokedex.css 	modified:   html/index.html 	modified:   js/main.js 	new file:   js/poke-api.js 	new file:   js/pokemon-model.js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemons ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                    alt= "${pokemon.name}">
+
+            </div>
+
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
+
